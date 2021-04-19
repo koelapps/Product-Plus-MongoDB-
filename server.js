ba5b95d82f8e04e61e4251b51e626bbfea76072b
MINGW64 <md.asad@koelapps.com>
2021-04-19 19:30:56 +0530

    for heroku

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get('/', (req, res) => res.send('welcome to product plus'));
 
 //Route Files
 app.use('/api/users', UserRoute);
-app.use('https://productplusapi.herokuapp.com/api/', AuthRoute);
+app.use('/api/', AuthRoute);
 
 
 
