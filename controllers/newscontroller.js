5555fbd772e80ae4641b6707d43b5bdd4ac736d0
MINGW64 <md.asad@koelapps.com>
2021-06-14 13:28:07 +0530

    node cron for news Feeds

--- controllers/newscontroller.js
+++ controllers/newscontroller.js
@@ -4,6 +4,7 @@ const xml2js = require('xml2js');
 const { maxHeaderSize } = require('http');
 const News = require('../models/News');
 const mainTags = require('../util/MainTags');
+const cron = require('node-cron');
 
 //channel Follow
 const channelFollow = asyncHandler(async (req, res, next) => {
@@ -81,21 +82,22 @@ const channelFollow = asyncHandler(async (req, res, next) => {
     newsFeed.push(object);
   });
 
-  let date = new Date().toString();
-  let count = feedLength;
-
   const connect = await News.create(newsFeed);
 
   const message = 'News Channels added Successfully....!';
 
-  res.json({
-    success: true,
-    // message: '',
-    // title: title,
-    // count: feedLength,
-    data: { message, title, feedLength, newsFeed },
-    // data: result,
-  });
+  if (corns === true) {
+    return null;
+  } else {
+    res.json({
+      success: true,
+      // message: '',
+      // title: title,
+      // count: feedLength,
+      data: { message, title, feedLength, newsFeed },
+      // data: result,
+    });
+  }
 });
 
 //channel unFollow
@@ -119,6 +121,11 @@ const paginateFeed = asyncHandler(async (req, res, next) => {
     .select()
     .limit(limit * 1);
   res.status(200).json({ success: true, data: results });
+});
+
+const corns = cron.schedule('00 05 * * *', function () {
+  channelFollow();
+  console.log('News Added ');
 });
 
 module.exports = {
